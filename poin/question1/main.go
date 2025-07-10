e7c157d967682daa95e86882bd2c24acf4b60d07
luoquan1982 <luo.quan1982@gmail.com>
2025-07-10 09:04:52 +0800

    完成指针练习第二题

--- poin/question1/main.go
+++ poin/question1/main.go
@@ -1,16 +1,15 @@
 package main
 
-func Add10(x *int) *int {
+func Add10(x *int) {
 	if x == nil {
 		panic("nil pointer dereference")
 	}
 	*x += 10
-	return x
 }
 
 func main() {
 	// Example usage
 	var num int = 5
-	result := Add10(&num)
-	println(*result) // Output: 15
+	Add10(&num)
+	println(num) // Output: 15
 }
---
+++ poin/question2/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func doubleSliceEle(d *[]int) {
+	if d == nil {
+		panic("nil slice dereference")
+	}
+	for i := range *d {
+		(*d)[i] *= 2
+	}
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5}
+	doubleSliceEle(&arr)
+	fmt.Println(arr) // Output: [2 4 6 8 10]
+}
